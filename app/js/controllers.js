e6c7a073be76f78f8513ebd2dc9f7c92b2e9d369
Akiva Bamberger <akiva.bamberger@gmail.com>
2013-10-07 10:17:07 -0700

    Updated comments.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,6 +9,9 @@ angular.module('Waze.controllers', ['Waze.services'])
     var editFunc = $compile('<div waze-edit></div>');
     var map = L.map('map');
 
+    /**
+     * Initial setup for the controller.
+     */
     $scope.initialSetup = function() {
       L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {}).addTo(map);
       map.locate({setView: true, maxZoom: 16});
@@ -37,6 +40,10 @@ angular.module('Waze.controllers', ['Waze.services'])
       });
     };
 
+    /**
+     * On click, create new notification.
+     * @param {L.MapEvent} e MapEvent.
+     */
     $scope.newNotification = function(e) {
       var notification = {
         lat: e.latlng.lat,
@@ -52,37 +59,28 @@ angular.module('Waze.controllers', ['Waze.services'])
       $scope.marker.openPopup();
     };
 
+    /**
+     * From data, adds all notifications from data to an array.
+     * @param {Array<Object>} data an array of notification data.
+     */
 
-    $scope.initialSetup();
-
-    $scope.setSelected = function(notification) {
-      if ($scope.opened == notification.id) {
-        $scope.opened = null;
-      } else {
-        $scope.opened = notification.id;
+    $scope.resetNotifications = function(data) {
+      for (var i = 0; i < data.length; ++i) {
+        if (!data[i] || !data[i].is_active) {
+          continue;
+        }
+        $scope.notifications.push(data[i]);
+        $scope.addNotification(data[i], NotificationService.Action.EDIT);
       }
     };
 
-    $scope.isSelected = function(notification) {
-      return $scope.opened == notification.id;
-    };
-
-    $scope.mapEdit = function(notification, marker) {
-      var scope = $scope.$new();
-      scope.notification = angular.copy(notification);
-      scope.marker = marker;
-      var compiled = editFunc(scope);
-      var popup =  L.popup({ minWidth: 250 }).setContent(compiled[0]);
-      marker.bindPopup(popup).openPopup();
-    };
-
-    $scope.request = function(action, notification, marker) {
-      if (marker) {
-        var latlng = marker.getLatLng();
-        notification.lat = latlng.lat;
-        notification.lon = latlng.lng;
-      }
-
+    /**
+     * Utility functions for interacting with the NotificationService, which is the model
+     * of the app.
+     * @param {string} action Action to take on Service.
+     * @param {Object} notification Notification to send request for.
+     */
+    $scope.request = function(action, notification) {
       NotificationService.request(action, notification).success(function(data){
         $scope.notice = 'done.';
         $timeout(function(){
@@ -93,6 +91,51 @@ angular.module('Waze.controllers', ['Waze.services'])
       })
     };
 
+    $scope.requestDelete = function(notification) {
+      $scope.notice = "deleting...";
+      $scope.request(NotificationService.Action.DELETE, notification);
+    };
+
+    $scope.requestEdit = function(notification, marker) {
+      $scope.notice = "editing...";
+      if (marker) {
+        var latlng = marker.getLatLng();
+        notification.lat = latlng.lat;
+        notification.lon = latlng.lng;
+      }
+      $scope.request(NotificationService.Action.EDIT, notification, marker);
+    };
+
+    $scope.requestUpvote = function(notification) {
+      $scope.notice = "upvoting...";
+      $scope.request(NotificationService.Action.UPVOTE, notification);
+    };
+
+    $scope.requestAdd = function(notification, marker) {
+      $scope.notice = "adding...";
+      if (marker) {
+        var latlng = marker.getLatLng();
+        notification.lat = latlng.lat;
+        notification.lon = latlng.lng;
+      }
+      $scope.request(NotificationService.Action.ADD, notification, marker);
+    };
+
+    /********************************************
+     * Functions for map view.
+     ********************************************/     
+    $scope.mapEdit = function(notification, marker) {
+      var scope = $scope.$new();
+      scope.notification = angular.copy(notification);
+      scope.marker = marker;
+      var compiled = editFunc(scope);
+      var popup =  L.popup({ minWidth: 250 }).setContent(compiled[0]);
+      marker.bindPopup(popup).openPopup();
+    };
+
+    /**
+     * Functions for interacting with a fullscreen popup, e.g. for confirmation.
+     */
     $scope.popupDelete = function(notification) {
       $scope.notification = angular.copy(notification);
       $scope.popup = {
@@ -123,46 +166,6 @@ angular.module('Waze.controllers', ['Waze.services'])
       };
     };
 
-    $scope.getDistance = function(notification) {
-      if (!$scope.mapReady || !notification)
-        return 0;
-      var center = map.getCenter();
-      var radlat1 = Math.PI * center.lat/180;
-      var radlat2 = Math.PI * notification.lat/180;
-      var radlon1 = Math.PI * center.lng/180;
-      var radlon2 = Math.PI * notification.lon/180;
-      var theta = center.lng - notification.lon;
-      var radtheta = Math.PI * theta/180;
-      var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-      dist = Math.acos(dist);
-      dist = dist * 180/Math.PI;
-      dist = dist * 60 * 1.1515;
-      return dist;
-    };
-
-    $scope.getCount = function() {
-      var visible = 0;
-      for (var i = 0; i < $scope.notifications.length; ++i) {
-        if ($scope.isVisible($scope.notifications[i])) {
-          ++visible;
-        }
-      }
-      var total = $scope.notifications.length;
-      if (visible < total) return visible + " of " + total;
-      return "all";
-    };
-
-    $scope.isVisible = function(notification) {
-      if (!$scope.mapReady || !notification)
-        return false;
-      var border = map.getBounds();
-      return border.contains(new L.LatLng(notification.lat, notification.lon));
-    };
-
-    $scope.center = function(notification) {
-      map.panTo(new L.LatLng(notification.lat, notification.lon), 8);
-    };
-
     $scope.showDeleteForm = function() {
       return $scope.popup && ($scope.popup.action == NotificationService.Action.DELETE);
     };
@@ -174,27 +177,6 @@ angular.module('Waze.controllers', ['Waze.services'])
     $scope.showUpvoteForm = function() {
       return $scope.popup && ($scope.popup.action == NotificationService.Action.UPVOTE);
     };
-
-    $scope.requestDelete = function(notification) {
-      $scope.notice = "deleting...";
-      $scope.request(NotificationService.Action.DELETE, notification);
-    };
-
-    $scope.requestEdit = function(notification, marker) {
-      $scope.notice = "editing...";
-      $scope.request(NotificationService.Action.EDIT, notification, marker);
-    };
-
-    $scope.requestUpvote = function(notification) {
-      $scope.notice = "upvoting...";
-      $scope.request(NotificationService.Action.UPVOTE, notification);
-    };
-
-    $scope.requestAdd = function(notification, marker) {
-      $scope.notice = "adding...";
-      $scope.request(NotificationService.Action.ADD, notification, marker);
-    };
-
 
     $scope.cancelAdd = function() {
       map.removeLayer($scope.marker);
@@ -249,13 +231,86 @@ angular.module('Waze.controllers', ['Waze.services'])
       return marker;
     };
 
-    $scope.resetNotifications = function(data) {
-      for (var i = 0; i < data.length; ++i) {
-        if (!data[i] || !data[i].is_active) {
-          continue;
-        }
-        $scope.notifications.push(data[i]);
-        $scope.addNotification(data[i], NotificationService.Action.EDIT);
+    /********************************************
+     * Functions for list view.
+     ********************************************/
+    /**
+     * Selects a notification to be viewed in the list view.
+     * @param {Object} notification object 
+     */
+    $scope.setSelected = function(notification) {
+      if ($scope.opened == notification.id) {
+        $scope.opened = null;
+      } else {
+        $scope.opened = notification.id;
       }
     };
+
+    /**
+     * Whether a notification is currently being opened in list view.
+     * @param {Object} notification object 
+     */
+    $scope.isSelected = function(notification) {
+      return $scope.opened == notification.id;
+    };
+
+    /**
+     * Returns the distance from a notification to the center of a map.
+     * @param {Object} notification object 
+     */
+    $scope.getDistance = function(notification) {
+      if (!$scope.mapReady || !notification)
+        return 0;
+      var center = map.getCenter();
+      var radlat1 = Math.PI * center.lat/180;
+      var radlat2 = Math.PI * notification.lat/180;
+      var radlon1 = Math.PI * center.lng/180;
+      var radlon2 = Math.PI * notification.lon/180;
+      var theta = center.lng - notification.lon;
+      var radtheta = Math.PI * theta/180;
+      var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+      dist = Math.acos(dist);
+      dist = dist * 180/Math.PI;
+      dist = dist * 60 * 1.1515;
+      return dist;
+    };
+
+    /**
+     * Returns the number of visible elements in a list, or "all" if
+     * all elements are visible.
+     * @return {string} "all" or "x of y"
+     */
+    $scope.getCount = function() {
+      var visible = 0;
+      for (var i = 0; i < $scope.notifications.length; ++i) {
+        if ($scope.isVisible($scope.notifications[i])) {
+          ++visible;
+        }
+      }
+      var total = $scope.notifications.length;
+      if (visible < total) return visible + " of " + total;
+      return "all";
+    };
+
+    /**
+     * True if notification appears on map.
+     * @param {Object} notification object.
+     */
+    $scope.isVisible = function(notification) {
+      if (!$scope.mapReady || !notification)
+        return false;
+      var border = map.getBounds();
+      return border.contains(new L.LatLng(notification.lat, notification.lon));
+    };
+
+    /**
+     * Centers the map at a notification's latlng
+     * @param {Object} notification object.
+     */
+    $scope.center = function(notification) {
+      map.panTo(new L.LatLng(notification.lat, notification.lon), 8);
+    };
+
+    // Call initial setup.
+    $scope.initialSetup();
   }]);
